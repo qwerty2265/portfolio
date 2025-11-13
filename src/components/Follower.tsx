7fa05634e500d4f94b2a3b236df863744d2e845b
MAXIM MUN <maksimmun007@gmail.com>
2025-11-14 03:14:20 +0900

    feat(components): add Follower component for mouse tracking effect

---
+++ src/components/Follower.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+
+export default function Follower() {
+  const followerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const moveFollower = (e: MouseEvent) => {
+      if (!followerRef.current) return;
+      gsap.to(followerRef.current, {
+        x: e.clientX - 3,
+        y: e.clientY - 3,
+        duration: 0.4,
+        ease: 'power3.out',
+      });
+    };
+
+    window.addEventListener('mousemove', moveFollower);
+
+    return () => {
+      window.removeEventListener('mousemove', moveFollower);
+    };
+  }, []);
+
+  return <div className="hidden sm:block fixed size-12 bg-carmine-pink/10 rounded-full pointer-events-none z-1000 -translate-x-1/2 -translate-y-1/2" ref={followerRef} />;
+};
