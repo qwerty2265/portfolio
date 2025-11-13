b062c6512d09db7cf5ecac83d89faf27db75a6d6
MAXIM MUN <maksimmun007@gmail.com>
2025-11-14 06:23:13 +0900

    style(Follower): adjust follower position offset and update background opacity

--- src/components/Follower.tsx
+++ src/components/Follower.tsx
@@ -8,8 +8,8 @@ export default function Follower() {
     const moveFollower = (e: MouseEvent) => {
       if (!followerRef.current) return;
       gsap.to(followerRef.current, {
-        x: e.clientX - 3,
-        y: e.clientY - 3,
+        x: e.clientX - 1,
+        y: e.clientY - 1,
         duration: 0.4,
         ease: 'power3.out',
       });
@@ -22,5 +22,5 @@ export default function Follower() {
     };
   }, []);
 
-  return <div className="hidden sm:block fixed size-12 bg-carmine-pink/10 rounded-full pointer-events-none z-1000 -translate-x-1/2 -translate-y-1/2" ref={followerRef} />;
+  return <div className="hidden sm:block fixed size-12 bg-carmine-pink/20 rounded-full pointer-events-none z-1000 -translate-x-1/2 -translate-y-1/2" ref={followerRef} />;
 };
