576260ab24a55f8703ed8ab83677db0178a36af7
MAXIM MUN <maksimmun007@gmail.com>
2025-11-17 15:51:19 +0900

    feat(BackendIconsList): add links to icons

--- src/features/home/components/BackendIconsList.tsx
+++ src/features/home/components/BackendIconsList.tsx
@@ -6,61 +6,99 @@ import { ICON_CLASSES } from "../constants";
 const icons = (
   <>
     <li>
-      <SiPhp title="PHP" className={ICON_CLASSES.default} />
+      <a href="https://www.php.net/" target="_blank" rel="noopener noreferrer">
+        <SiPhp title="PHP" className={ICON_CLASSES.default} />
+      </a>
     </li>
     <li>
-      <SiGo title="Go" className={ICON_CLASSES.featured} />
+      <a href="https://go.dev/" target="_blank" rel="noopener noreferrer">
+        <SiGo title="Go" className={ICON_CLASSES.featured} />
+      </a>
     </li>
     <li>
-      <SiLaravel title="Laravel" className={ICON_CLASSES.default} />
+      <a href="https://laravel.com/" target="_blank" rel="noopener noreferrer">
+        <SiLaravel title="Laravel" className={ICON_CLASSES.default} />
+      </a>
     </li>
     <li>
-      <SiExpress title="Express.js" className={ICON_CLASSES.default} />
+      <a href="https://expressjs.com/" target="_blank" rel="noopener noreferrer">
+        <SiExpress title="Express.js" className={ICON_CLASSES.default} />
+      </a>
     </li>
     <li>
-      <SiPython title="Python" className={ICON_CLASSES.featured} />
+      <a href="https://www.python.org/" target="_blank" rel="noopener noreferrer">
+        <SiPython title="Python" className={ICON_CLASSES.featured} />
+      </a>
     </li>
     <li>
-      <SiMysql title="MySQL" className={ICON_CLASSES.default} />
+      <a href="https://www.mysql.com/" target="_blank" rel="noopener noreferrer">
+        <SiMysql title="MySQL" className={ICON_CLASSES.default} />
+      </a>
     </li>
     <li>
-      <DiMongodb title="MongoDB" className={ICON_CLASSES.default} />
+      <a href="https://www.mongodb.com/" target="_blank" rel="noopener noreferrer">
+        <DiMongodb title="MongoDB" className={ICON_CLASSES.default} />
+      </a>
     </li>
     <li>
-      <SiPostgresql title="PostgreSQL" className={ICON_CLASSES.featured} />
+      <a href="https://www.postgresql.org/" target="_blank" rel="noopener noreferrer">
+        <SiPostgresql title="PostgreSQL" className={ICON_CLASSES.featured} />
+      </a>
     </li>
     <li>
-      <SiSqlite title="SQLite" className={ICON_CLASSES.default} />
+      <a href="https://www.sqlite.org/" target="_blank" rel="noopener noreferrer">
+        <SiSqlite title="SQLite" className={ICON_CLASSES.default} />
+      </a>
     </li>
     <li>
-      <SiJavascript title="JavaScript" className={ICON_CLASSES.default} />
+      <a href="https://developer.mozilla.org/en-US/docs/Glossary/JavaScript" target="_blank" rel="noopener noreferrer">
+        <SiJavascript title="JavaScript" className={ICON_CLASSES.default} />
+      </a>
     </li>
     <li>
-      <SiNodedotjs title="Node.js" className={ICON_CLASSES.featured} />
+      <a href="https://nodejs.org/" target="_blank" rel="noopener noreferrer">
+        <SiNodedotjs title="Node.js" className={ICON_CLASSES.featured} />
+      </a>
     </li>
     <li>
-      <SiDocker title="Docker" className={ICON_CLASSES.default} />
+      <a href="https://www.docker.com/" target="_blank" rel="noopener noreferrer">
+        <SiDocker title="Docker" className={ICON_CLASSES.default} />
+      </a>
     </li>
     <li>
-      <SiPrisma title="Prisma" className={ICON_CLASSES.default} />
+      <a href="https://www.prisma.io/" target="_blank" rel="noopener noreferrer">
+        <SiPrisma title="Prisma" className={ICON_CLASSES.default} />
+      </a>
     </li>
     <li>
-      <SiTypescript title="TypeScript" className={ICON_CLASSES.featured} />
+      <a href="https://www.typescriptlang.org/" target="_blank" rel="noopener noreferrer">
+        <SiTypescript title="TypeScript" className={ICON_CLASSES.featured} />
+      </a>
     </li>
     <li>
-      <SiJsonwebtokens title="JSON Web Tokens" className={ICON_CLASSES.default} />
+      <a href="https://jwt.io/" target="_blank" rel="noopener noreferrer">
+        <SiJsonwebtokens title="JSON Web Tokens" className={ICON_CLASSES.default} />
+      </a>
     </li>
     <li>
-      <SiAuth0 title="Auth0" className={ICON_CLASSES.default} />
+      <a href="https://auth0.com/" target="_blank" rel="noopener noreferrer">
+        <SiAuth0 title="Auth0" className={ICON_CLASSES.default} />
+      </a>
     </li>
     <li>
-      <SiSupabase title="Supabase" className={ICON_CLASSES.featured} />
+      <a href="https://supabase.com/" target="_blank" rel="noopener noreferrer">
+        <SiSupabase title="Supabase" className={ICON_CLASSES.featured} />
+      </a>
     </li>
     <li>
-      <SiFirebase title="Firebase" className={ICON_CLASSES.default} />
+      <a href="https://firebase.google.com/" target="_blank" rel="noopener noreferrer">
+        <SiFirebase title="Firebase" className={ICON_CLASSES.default} />
+      </a>
     </li>
     <li>
-      <SiDjango title="Django" className={ICON_CLASSES.default} />
+      <a href="https://www.djangoproject.com/" target="_blank" rel="noopener noreferrer">
+        <SiDjango title="Django" className={ICON_CLASSES.default} />
+      </a>
     </li>
   </>
 )
