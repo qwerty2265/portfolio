38b1bb328d6ad9c014c926bd66ed6df900f654a7
MAXIM MUN <maksimmun007@gmail.com>
2026-03-14 08:40:01 +0900

    feat(styles): add getThemeColor function to retrieve theme colors with fallback

--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getThemeColor(token: string, fallback = "#fff") {
+  const rootStyles = getComputedStyle(document.documentElement);
+  const value = rootStyles.getPropertyValue(token).trim();
+  return value || fallback;
+};
