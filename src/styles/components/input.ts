588158a12fb8124704d0c90eb4d5f6c675702c90
MAXIM MUN <maksimmun007@gmail.com>
2026-03-26 13:40:35 +0900

    feat(Input, LabeledInput, LabeledTextArea, TextArea): add input components with labels and character count functionality

---
+++ src/styles/components/input.ts
@@ -0,0 +1,3 @@
+export const inputBaseClass = "px-5 py-4 border border-cultured rounded-2xl focus:border-carmine-pink focus:ring-0 focus:outline-none transition-all duration-300";
+
+export const labelBaseClass = "absolute px-1 text-md left-3 top-0 -translate-y-1/2 text-cultured bg-charcoal-gray pointer-events-none transition-all";
